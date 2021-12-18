00da4518c59540469c3ed7a4b3df51c121049e87
jellrina <dlgh6007@gmail.com>
2021-12-18 22:49:26 +0900

    사용자 정보 입력창

--- app/src/main/java/com/example/userinfo/MainActivity.java
+++ app/src/main/java/com/example/userinfo/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
+@SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity {
 
     TextView tvName, tvEmail, toastText;
@@ -31,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         tvEmail = (TextView) findViewById(R.id.tvEmail);
         btn1 = (Button) findViewById(R.id.btn1);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(new View.OnClickListener() { //여기를 클릭 버튼 메뉴 클릭 시
             @Override
             public void onClick(View v) {
                 dialogView = (View) View.inflate(MainActivity.this, R.layout.activity_dialog1, null); //다이얼로그뷰에 인플레이트 하여 대입
@@ -39,12 +42,17 @@ public class MainActivity extends AppCompatActivity {
                 dlg.setTitle("사용자 정보 입력");
                 dlg.setIcon(R.drawable.ic_menu_allfriends); //대화상자 아이콘 설정
                 dlg.setView(dialogView); //인플레이트 한 뷰를 대화상자에 적용
+                /*dlgEdtName = (EditText) dialogView.findViewById(R.id.dlgEdt1);
+                dlgEdtEmail = (EditText) dialogView.findViewById(R.id.dlgEdt2);
+                dlgEdtName.setText(tvName.getText().toString());
+                dlgEdtEmail.setText(tvEmail.getText().toString());*/
+
 
                 dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        dlgEdtName = (EditText) dialogView.findViewById(R.id.dlgEdt1);
-                        dlgEdtEmail = (EditText) dialogView.findViewById(R.id.dlgEdt2); //인플레이트한 다이얼로그뷰에 다이얼로그.xml 변수 대입후 메소드 사용
+                       dlgEdtName = (EditText) dialogView.findViewById(R.id.dlgEdt1);
+                       dlgEdtEmail = (EditText) dialogView.findViewById(R.id.dlgEdt2); //인플레이트한 다이얼로그뷰에 다이얼로그.xml 변수 대입후 메소드 사용
 
                         tvName.setText(dlgEdtName.getText().toString());
                         tvEmail.setText(dlgEdtEmail.getText().toString()); //대화상자를 입력한 이름과 이메일을 메인의 텍스트뷰에 적용
@@ -54,7 +62,13 @@ public class MainActivity extends AppCompatActivity {
                 dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast toast = new Toast(MainActivity.this); //빈 토스트 생성
+                        Toast toast = new Toast(MainActivity.this); //빈 토스트
+
+                        Display display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+                        int xOffset = (int)(Math.random() * display.getWidth());
+                        int yOffset = (int)(Math.random() * display.getHeight());
+                        toast.setGravity(Gravity.TOP | Gravity.LEFT, xOffset,yOffset); //
+
                         toastView = (View) View.inflate(MainActivity.this,R.layout.activity_toast1, null); //토스트뷰 파일을 인플레이트하여 뷰에 대입
                         toastText = (TextView) toastView.findViewById(R.id.toastText); //토스트뷰를 토스트텍스트와와대입 후 보여지기
                         toastText.setText("취소했습니다");
